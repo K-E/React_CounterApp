99629f83dc8b51eccd150c25da47cbbc68ba69b3
K-E <kesivakumar@gmail.com>
2023-10-11 16:21:18 +0530

    Change by : Sivakumar K.E Notes : Update to CSS file 	Calling the parent function directly from CounterButton, without 	an intermediate function.

--- src/CounterButtons.js
+++ src/CounterButtons.js
@@ -4,18 +4,11 @@ import {PropTypes} from 'prop-types'
 
 export default function CounterButtons({by, parentIncrement, parentDecrement}){
 
-    function increment(){
-        parentIncrement(by)
-    }
-    function decrement(){
-        parentDecrement(by)
-    }
-
     return(
         <div className = "">
-            <button onClick={increment} className="cButton">+{by}</button>
+            <button onClick={() => parentIncrement(by)} className="cButton">+{by}</button>
             <span></span>
-            <button onClick={decrement} className="cButton">-{by}</button>
+            <button onClick={() => parentDecrement(by)} className="cButton">-{by}</button>
         </div>
     )
 }
